4d37c4dfa265cfa1ad90aae4c78febfa04006bb0
ABDUL MUHAIMIN <m3tasploit@pop-os.localdomain>
2022-09-20 22:17:44 +0530

    added basic component skeleton and routing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import CreateUser from "./pages/CreateUser";
+import Users from "./pages/Users";
+import SingleUser from "./pages/SingleUser";
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Redirect to="/users" />
+          </Route>
+          <Route exact path="/users">
+            <Users />
+          </Route>
+          <Route exact path="/users/create">
+            <CreateUser />
+          </Route>
+          <Route exact path="/users/:id">
+            <SingleUser />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
