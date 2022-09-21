b32dc287c8e3c820f43a39bab8ef36c0bb2565e1
ABDUL MUHAIMIN <m3tasploit@pop-os.localdomain>
2022-09-21 15:28:41 +0530

    add avatar url display, add last and first page button

--- src/components/UserCard/UserCard.jsx
+++ src/components/UserCard/UserCard.jsx
@@ -1,17 +1,27 @@
 import React from "react";
 import classes from "./UserCard.module.css";
 
-function UserCard({ name, age, createdAt, statusMessage, isPublic }) {
+function UserCard({
+  name,
+  age,
+  createdAt,
+  statusMessage,
+  isPublic,
+  avatarUrl,
+}) {
   return (
     <div className={classes.card}>
       <div className={classes.col}>
-        <div className={classes.dp} />
+        <div
+          className={classes.dp}
+          style={{ backgroundImage: `url(${avatarUrl})` }}
+        />
         <p>Age : {age}</p>
       </div>
       <div className={classes.col}>
         <h4>Name : {name}</h4>
         <p>{statusMessage}</p>
-        <p>Created at : {new Date(createdAt).toLocaleString()}</p>
+        <p>{new Date(createdAt).toLocaleString()}</p>
       </div>
       <div className={classes.icon}>
         {!isPublic && <div className={classes.line} />}
--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -8,12 +8,12 @@ let limit = 4;
 
 function Users() {
   const [users, setUsers] = useState([]);
-  const [page, setPage] = useState(1);
+  const [page, setPage] = useState(0);
   const [pageCount, setPageCount] = useState(0);
 
   useEffect(() => {
     (async () => {
-      const { data, headers } = await fetchUsers(page, limit);
+      const { data, headers } = await fetchUsers(page + 1, limit);
       if (headers["x-total-count"]) {
         let total_count = headers["x-total-count"];
         let pagecount = Math.ceil(total_count / limit);
@@ -28,7 +28,7 @@ function Users() {
   };
 
   return (
-    <div style={{ padding: "2rem 2rem" }}>
+    <div style={{ padding: "0rem 2rem" }}>
       <h1 style={{ textAlign: "center" }}>Users</h1>
       <div
         style={{
@@ -43,6 +43,9 @@ function Users() {
         ))}
       </div>
       <div className={classes.container}>
+        <div className={classes.leftArrow} onClick={() => setPage(0)}>
+          {"<<"}
+        </div>
         <ReactPaginate
           breakLabel="..."
           nextLabel=">"
@@ -52,7 +55,16 @@ function Users() {
           previousLabel="<"
           renderOnZeroPageCount={null}
           activeClassName={classes.activePage}
+          forcePage={page}
         />
+        <div
+          className={classes.rightArrow}
+          onClick={() => {
+            setPage(() => (pageCount > 0 ? pageCount - 1 : pageCount));
+          }}
+        >
+          {">>"}
+        </div>
       </div>
     </div>
   );
