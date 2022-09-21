c9347a59a13b4c677d2cb804963cce0ce4cc1b2b
ABDUL MUHAIMIN <m3tasploit@pop-os.localdomain>
2022-09-21 13:21:37 +0530

    implemented pagination

--- src/api/user.js
+++ src/api/user.js
@@ -1,9 +1,10 @@
 import axios from "axios";
 const client = axios.create({
-  baseURL: "http://localhost:3333/users",
+  baseURL: "http://localhost:3333",
 });
-export const fetchUsers = () => {
-    return client.get("/")
+
+export const fetchUsers = (page, limit) => {
+  return client.get("/users", { params: { _page: page, _limit: limit } });
 };
 
 export const updateUser = () => {};
--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -1,20 +1,34 @@
 import React, { useEffect, useState } from "react";
+import ReactPaginate from "react-paginate";
 import { fetchUsers } from "../api/user";
 import UserCard from "../components/UserCard/UserCard";
+import classes from "./Users.module.css";
+
+let limit = 4;
 
 function Users() {
   const [users, setUsers] = useState([]);
-  console.log({ users });
+  const [page, setPage] = useState(1);
+  const [pageCount, setPageCount] = useState(0);
 
   useEffect(() => {
     (async () => {
-      const { data } = await fetchUsers();
+      const { data, headers } = await fetchUsers(page, limit);
+      if (headers["x-total-count"]) {
+        let total_count = headers["x-total-count"];
+        let pagecount = Math.ceil(total_count / limit);
+        setPageCount(pagecount);
+      }
       setUsers(data);
     })();
-  }, []);
+  }, [page]);
+
+  const handlePageClick = (event) => {
+    setPage(event.selected);
+  };
 
   return (
-    <div>
+    <div style={{ padding: "2rem 2rem" }}>
       <h1 style={{ textAlign: "center" }}>Users</h1>
       <div
         style={{
@@ -27,6 +41,18 @@ function Users() {
         {users.map((user) => (
           <UserCard {...user} key={user.id.toString()} />
         ))}
+      </div>
+      <div className={classes.container}>
+        <ReactPaginate
+          breakLabel="..."
+          nextLabel=">"
+          onPageChange={handlePageClick}
+          pageRangeDisplayed={5}
+          pageCount={pageCount}
+          previousLabel="<"
+          renderOnZeroPageCount={null}
+          activeClassName={classes.activePage}
+        />
       </div>
     </div>
   );
